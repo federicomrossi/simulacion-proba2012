c35b50272771ea75be94c2b703b95b6ed2f517f6
federicomrossi@gmail.com <federicomrossi@gmail.com@5ea5b284-5da4-099b-848d-9d49faa16a0b>
2012-05-12 17:58:42 +0000

    Avance en el informe. Agregado de documentación en algunos codigos fuente

--- trunk/source/probDobleDelOtro.m
+++ trunk/source/probDobleDelOtro.m
@@ -1,10 +1,19 @@
+# Funcion que estima la probabilidad de que al tirar dos dados, uno tenga
+# el doble de valor que el otro.
+# PRE: Debe pasarse por parametro dos vectores que sean los valores obtenidos
+# en dos tiradas distintas de dados, cuyos valores se obtienen utilizando la
+# funcion randomDado()
+# POST: Se devuelve la probabilidad.
 function y = probDobleDelOtro(tiradasDado1,tiradasDado2)
 
 	cantDobleDoble = 0;
+	
 	for i= 1:length(tiradasDado1)
 		if( tiradasDado1(i) == 2*tiradasDado2(i))
 			cantDobleDoble = cantDobleDoble + 1;
 		endif	
 	endfor
+    
 	y = cantDobleDoble/length(tiradasDado1);
+	
 endfunction
