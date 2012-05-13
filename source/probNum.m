40cc67c4c87112c999e19a60f5554b90ede64df9
federicomrossi@gmail.com <federicomrossi@gmail.com@5ea5b284-5da4-099b-848d-9d49faa16a0b>
2012-05-13 01:15:37 +0000

    Actualización en los archivos del repositorio. Desde ahora no subimos archivos de compilacion, ya sea de Latex o de Octave.

--- source/probNum.m
+++ source/probNum.m
@@ -1,13 +1,17 @@
 # Funcion que estima la probabilidad de que aparezca un numero en un vector
-# PRE: Debe pasarse por parametro  el numero 'num', que es el numero del que se quiere calcular la probabilidad, y 
-# el 'vector' de numeros.
+# PRE: Debe pasarse por parametro  el numero 'num', que es el numero del que se 
+# quiere calcular la probabilidad, y el 'vector' de numeros.
 # POST: Se devuelve la probabilidad.
 function y = probNum(num,vector)
+	
 	cantApariciones = 0;
+	
 	for j = 1:length(vector)
 		if(vector(j) == num)
 			cantApariciones = cantApariciones+1;
 		endif
 	endfor
+
 	y = cantApariciones/length(vector);
+	
 endfunction	
