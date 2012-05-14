dc3bb37e33d2fd5dcaf74c76fdec56d570ccebd9
federicomrossi@gmail.com <federicomrossi@gmail.com@5ea5b284-5da4-099b-848d-9d49faa16a0b>
2012-05-14 04:03:48 +0000

    Apéndice mejorado

---
+++ trunk/source/probDeTodasLasLongitudes.m
@@ -0,0 +1,24 @@
+# Funcion que calcula la probabilidad de todas las longitudes 
+# de una secuencia dentro de una lista de secuencias .
+# PRE: Debe pasarse por parametro la listaDeSecuencias de las 
+# que se quiere calcular la probabilidades.
+# POST: Se devuelve una matriz con la longitud y su respectiva 
+# probabilidad, por fila.
+
+function y = probDeLaLongitud(longitud,listaDeSecuencias) 
+	
+	vectorDeLongitudesQueYaAparecieron = [];
+	matriz = [];
+	
+	cantDeSecuencias = length(listaDeSecuencias);
+	#Recorre la listaDeSecuencias y guarda en la matriz la long de la secuencia y su probabilidad.
+	for i = 1:cantDeSecuencias
+		longDeLaSecuencia = length(listaDeSecuencias{i});
+		if  (!ismember(longDeLaSecuencia,vectorDeLongitudesQueYaAparecieron))
+			matriz = [ matriz longDeLaSecuencia probDeLaLongitud(longDeLaSecuencia,listaDeSecuencias)];
+			vectorDeLongitudesQueYaAparecieron = [vectorDeLongitudesQueYaAparecieron longDeLaSecuencia];
+		endif
+    	endfor
+	y = matriz;
+	
+endfunction
