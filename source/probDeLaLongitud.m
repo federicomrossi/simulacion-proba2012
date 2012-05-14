c60ae8d7c7db056d5be723b700a48e4c7c4fb90a
federicomrossi@gmail.com <federicomrossi@gmail.com@5ea5b284-5da4-099b-848d-9d49faa16a0b>
2012-05-14 03:29:29 +0000

    Agregada seccion 3.3

---
+++ source/probDeLaLongitud.m
@@ -0,0 +1,20 @@
+# Funcion que calcula la probabilidad de la longitud de una secuencia dentro de 
+# una lista de secuencias .
+# PRE: Debe pasarse por parametro la listaDeSecuencias, y la longitud de la que 
+# se quiere calcular la probabilidad.
+# POST: Se devuelve la probabilidad.
+
+function y = probDeLaLongitud(longitud,listaDeSecuencias) 
+
+	cantApariciones = 0;	
+	cantDeSecuencias = length(listaDeSecuencias);
+	
+	#Recorre la listaDeSecuencias y cuenta la cantidad de secuencias con la misma longitud.
+	for i = 1:cantDeSecuencias
+		if  (longitud == length(listaDeSecuencias{i}))
+			cantApariciones = cantApariciones + 1;
+	endfor
+    
+	y = cantApariciones/cantDeSecuencias;
+	
+endfunction
