d8cb06acfc4abd0b52800cc34421087d5f6464a9
zchonny@gmail.com <zchonny@gmail.com@5ea5b284-5da4-099b-848d-9d49faa16a0b>
2012-06-23 01:52:31 +0000

    corregido el punto 2.4

--- source/probAsEn4Tiradas.m
+++ source/probAsEn4Tiradas.m
@@ -1,19 +1,18 @@
-# Funcion calcula la probabilidad de que salga un as en 4 tiradas de dos daods.
+# Funcion calcula la probabilidad de que salga un as en 4 tiradas de un dado.
 # PRE: 'n' es la cantidad de repeticiones de la experiencia.
 # POST: Se devuelve la probabilidad.
 function y = probAsEn4Tiradas(n)
 
 	cantTiradasEnLasQueSalioAs = 0;
 	
-	#Ciclo que repite n veces las 4 tiradas de dos dados. 
+	#Ciclo que repite n veces las 4 tiradas de un dado. 
 	for i = 1:n
 		
 		salioAs = false;
 		d1 = randomDado(4);
-		d2 = randomDado(4);
 		
 		for j = 1:4
-			if(d1(j)== 1 || d2(j) == 1)
+			if(d1(j)== 1)
 				#Salio al menos un as.
 				salioAs = true;
 			endif	
